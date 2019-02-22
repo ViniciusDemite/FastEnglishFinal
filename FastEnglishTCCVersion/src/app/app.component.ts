84f3997fb6b964eea6817667b1fb131be6fa88bd
Vinicius Demite Gubolin <vinicius.demite@outlook.com>
2019-02-22 20:25:09 -0300

    menu, home professor e atividades cad

--- FastEnglishTCCVersion/src/app/app.component.ts
+++ FastEnglishTCCVersion/src/app/app.component.ts
@@ -1,44 +1,52 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, IonicPage } from 'ionic-angular';
 
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Nav } from 'ionic-angular';
 import { ProfessorHomePage } from "../pages/professor-home/professor-home";
-
-
+import { EstudanteHomePage } from '../pages/estudante-home/estudante-home';
+import { InicialPage } from '../pages/inicial/inicial';
+// @IonicPage()
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   
-  nav: Nav;
-  pages: Array<{ title: string, component: string, openTab?: any }>;
+  @ViewChild('content') nav: Nav;
+  pages: Array<{ icon: string, title: string, component: string }>;
   rootPage = ProfessorHomePage;
 
   constructor(public platform: Platform, public afAuth: AngularFireAuth) {
 
-    // platform.ready().then(() => {
+    platform.ready().then(() => {
 
-    //   // método para autenticar o status do usuário
-    //   afAuth.auth.onAuthStateChanged((user) => {
+      // afAuth.auth.onAuthStateChanged((user) => {
 
-    //     if (user != null) { this.rootPage = EstudanteHomePage; } //logado
+      //   if (user != null) {
 
-    //     else { this.rootPage = InicialPage; } //deslogado
+      //     if (!user.uid.check) {
+      //       this.rootPage = ProfessorHomePage;
+      //     } else {
+      //       this.rootPage = EstudanteHomePage;
+      //     }
 
-    //   })
+      //   } else {
+      //     this.rootPage = InicialPage;
+      //   }
 
-    // });
+      // })
 
-    this.pages = [
-      { title: 'Ativities', component: 'AtividadesCadPage', openTab: 1 },
-      // { title: 'Atividades', component: 'EstudanteAtividadesPage', openTab: 1 } ,
-      // { title: 'Professor', component: 'EstudanteProfessorPage' },
-    ];
+      this.pages = [
+        { icon: 'clipboard', title: 'Ativities', component: 'AtividadesCadPage' },
+        { icon: 'person', title: 'Profile', component: 'ProfessorProfilePage' } ,
+        { icon: 'people', title: 'Students', component: 'ProfessorEstudantesPage' },
+      ];
+
+    });
 
   }
 
   public openPage(page) {
-    this.nav.setRoot(page.component, { openTab: page.openTab });
+    this.nav.setRoot(page.component);
   }
 }
