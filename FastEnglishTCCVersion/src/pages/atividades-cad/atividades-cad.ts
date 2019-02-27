0f2a97ab5ee5a4644e1ddee034ce90432e031029
Vinicius Demite Gubolin <vinicius.demite@outlook.com>
2019-02-27 18:21:38 -0300

    tentativa de cad audio

--- FastEnglishTCCVersion/src/pages/atividades-cad/atividades-cad.ts
+++ FastEnglishTCCVersion/src/pages/atividades-cad/atividades-cad.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Media, MediaObject } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
 
 @Component({
   selector: 'page-atividades-cad',
@@ -9,7 +11,15 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class AtividadesCadPage {
 
-  constructor(public navCtrl: NavController, public db: AngularFirestore, public alertCtrl: AlertController) {
+  public recording: boolean = false;
+  public recorded: boolean = false;
+  public filePath: string;
+  public fileName: string;
+  public audio: MediaObject;
+  public audioList: any[] = [];
+
+  constructor(public navCtrl: NavController, public db: AngularFirestore,
+    public alertCtrl: AlertController) {
   }
 
   public cad_atividade(form: NgForm): void {
@@ -52,5 +62,50 @@ export class AtividadesCadPage {
     }
     
   }
+
+  // private media: Media
+  // private file: File
+  // public start_audio(): void {
+  //   this.fileName = 'record' + new Date().getTime() + '.3gp';
+  //   this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+  //   this.audio = this.media.create(this.filePath);
+  //   this.audio.startRecord();
+  //   this.recording = true;
+  // }
+
+  // public stop_audio(): void {
+  //   this.audio.stopRecord();
+  //   // let data = { filename: this.fileName };
+  //   // this.audioList.push(data);
+  //   // localStorage.setItem("audiolist", JSON.stringify(this.audioList));
+  //   this.recording = false;
+  //   this.recorded = true;
+  // }
+
+  // public play_audio(): void {
+  //   this.audio.play();
+  //   this.audio.setVolume(1.0);
+  // }
+
+  /************** Exemplo funcional sauvar audio *********************/
+  // Uri uriAudio = Uri.fromFile(new File(audioFilePath).getAbsoluteFile());
+  // final StorageReference filePath = ref.child("Education/image").child(uriImage.getLastPathSegment());
+  // final StorageReference audioRef = ref.child("Education/audio").child(uriAudio.getLastPathSegment());
+
+
+  // // on success upload audio
+  // audioRef.putFile(uriAudio).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot> () {
+  // @Override
+  // public void onSuccess(final UploadTask.TaskSnapshot audioSnapshot) {
+
+  //   //upload image
+  //   filePath.putFile(uriImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+  //                           @Override
+  //   public void onSuccess(final UploadTask.TaskSnapshot imageSnapshot) {
+
+  //     mProgress.dismiss();
+
+  //     @SuppressWarnings("VisibleForTests") Uri audioUrl = audioSnapshot.getDownloadUrl();
+  //     @SuppressWarnings("VisibleForTests") Uri imageUrl = imageSnapshot.getDownloadUrl();
 
 }
